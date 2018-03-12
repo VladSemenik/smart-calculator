787589c1997636b31e162c155096f347f74c02c7
Vlad Semenik <vlad.semenik@gmail.com>
2018-03-12 15:13:44 +0300

    'solution'

--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
+      this.variable = ''+initialValue;
   }
 
   add(number) {
-    // your implementation
+      this.variable += String('+'+number);
+      return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.variable += String('-'+number);
+      return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.variable += String('*'+number);
+      return this;
   }
 
   devide(number) {
-    // your implementation
+      this.variable += String('/'+number);
+      return this;
   }
 
   pow(number) {
-    // your implementation
+    this.variable += String('**'+number);
+      return this;
   }
+    valueOf(){
+        return eval(this.variable);
+    }
 }
 
 module.exports = SmartCalculator;
